b45ff36077bb2f491c1d7bfa91b494a43850a03d
YouJaeMin <wovkek@naver.com>
2016-10-13 12:02:30 +0900

    1

--- spring_04_jdbc/src/main/java/part04_daoSuppart/MemDaoImp.java
+++ spring_04_jdbc/src/main/java/part04_daoSuppart/MemDaoImp.java
@@ -2,7 +2,7 @@ package part04_daoSuppart;
 
 import java.util.List;
 
-import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 /*
  * Template 클래스
@@ -12,46 +12,40 @@ import org.mybatis.spring.SqlSessionTemplate;
  * 		사용함으로써 try~cath~finally 및 커넥션 관리를 위한 중복된 코드를 줄이거나 없앨 수 있다.
  */
 
-public class MemDaoImp implements MemDAO {
-
-	private SqlSessionTemplate sqlSessionTemplate;
+public class MemDaoImp extends SqlSessionDaoSupport implements MemDAO {
 
 	public MemDaoImp() {
 
 	}
 
-	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
-		this.sqlSessionTemplate = sqlSessionTemplate;
-	}
-
 	@Override
 	public List<MemDTO> list() {
-		return sqlSessionTemplate.selectList("mem2.all");
+		return getSqlSession().selectList("mem2.all");
 	}
 
 	@Override
 	public void insertMethod(MemDTO dto) {
-		sqlSessionTemplate.insert("mem2.ins", dto);
+		getSqlSession().insert("mem2.ins", dto);
 	}
 
 	@Override
 	public void updateMethod(MemDTO dto) {
-		sqlSessionTemplate.update("mem2.upd", dto);
+		getSqlSession().update("mem2.upd", dto);
 	}
 
 	@Override
 	public void deleteMethod(int num) {
-		sqlSessionTemplate.delete("mem2.del", num);
+		getSqlSession().delete("mem2.del", num);
 	}
 
 	@Override
 	public MemDTO one(int num) {
-		return sqlSessionTemplate.selectOne("mem2.one", num);
+		return getSqlSession().selectOne("mem2.one", num);
 	}
 
 	@Override
 	public int countMethod() {
-		return sqlSessionTemplate.selectOne("mem2.cnt");
+		return getSqlSession().selectOne("mem2.cnt");
 	}
 
 }
