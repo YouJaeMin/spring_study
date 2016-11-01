6c65ce149c0b20ddd892d0b9d73413ea2b3d950b
YouJaeMin <wovkek@naver.com>
2016-11-01 15:12:21 +0900

    dd

--- spring_13_interceptor/src/main/java/part02/controller/UserController.java
+++ spring_13_interceptor/src/main/java/part02/controller/UserController.java
@@ -10,32 +10,35 @@ import part02.dto.PersonDTO;
 
 @Controller
 public class UserController {
-	
+
 	public UserController() {
 	}
-	
+
 	@RequestMapping("/login.do")
-	public String loginProcess(){
+	public String loginProcess() {
 		return "part02/loginForm";
 	}
-	
+
 	@RequestMapping("/logpro.do")
-	public String logCheckProcess(PersonDTO dto, HttpSession session, HttpServletRequest request){
-		if(dto.getId().equals("kim") && dto.getPass().equals("1234")){
+	public String logCheckProcess(PersonDTO dto, HttpSession session, HttpServletRequest request) {
+		if (dto.getId().equals("kim") && dto.getPass().equals("1234")) {
 			session.setAttribute("chk", dto.getId());
-			if(request.getParameter("returnUrl") != null){
-				return "redirect:/"+request.getParameter("returnUrl");
+
+			String returnUrl = request.getParameter("returnUrl");
+			if (returnUrl != "") {
+				System.out.println("11==" + returnUrl + "====");
+				return "redirect:/" + returnUrl;
 			}
 			return "redirect:/index.do";
-		}else{
-			return "redirect:/login.do";
+		} else {
+			return "redirect:/index.do";
 		}
 	}
-	
+
 	@RequestMapping("/logout.do")
-	public String logoutProcess(HttpSession session){
+	public String logoutProcess(HttpSession session) {
 		session.removeAttribute("chk");
 		return "redirect:index.do";
 	}
-	
+
 }
